101ced4241bf57b7a17c598e59b9d7b8642dc1cc
jeanyvesruffin <ruffinmax@hotmail.fr>
2021-01-04 11:59:26 +0100

    test xDebug

---
+++ tests.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma page web</title>
+</head>
+
+<body>
+    <h1>Ma page web</h1>
+    <p> Cette ligne a été écrite entièrement en HTML.<br />
+        <?php 
+        $testVariable=true;
+        echo "Celle-ci a été écrite entièrement en PHP."; 
+        echo "test XDebug";?>
+    </p>
+
+</body>
+
+</html>
