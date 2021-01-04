95e373017db244c6736345fd8243f812010289e0
jeanyvesruffin <ruffinmax@hotmail.fr>
2021-01-04 17:09:15 +0100

    Les variables

--- tests.php
+++ tests.php
@@ -10,10 +10,17 @@
 <body>
     <h1>Ma page web</h1>
     <p> Cette ligne a été écrite entièrement en HTML.<br />
-        <?php 
-        $testVariable=true;
-        echo "Celle-ci a été écrite entièrement en PHP."; 
-        echo "test XDebug";?>
+        <?php
+        // commentaire test
+        echo "Celle-ci a été écrite entièrement en PHP.";
+        echo "<br/>";
+        echo "Avec du dynamique nous sommes le ";
+        echo date('d/m/Y h:i:s');
+        /*
+            bloc de commentaires
+        */
+
+        ?>
     </p>
 
 </body>
